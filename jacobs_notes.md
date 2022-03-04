541c033d07ce912e7645346f14b840fb4546a485
jyablonski9 <jyablonski9@gmail.com>
2022-03-03 18:57:08 -0600

    reformat project

--- jacobs_notes.md
+++ jacobs_notes.md
@@ -6,3 +6,7 @@ run `mlflow ui` and go to `http://localhost:5000/#/` to view historical runs.  i
 
 `mlflow run sklearn_elasticnet_wine -P alpha=0.5`
 `mlflow run https://github.com/mlflow/mlflow-example.git -P alpha=5.0`
+
+
+have the tonights ml game have the odds data and have it look exactly like it does in prod, and then in 
+shiny make a selector to either select 1) tonight's games, or 2) the full remaining schedule.
--- src/nba_tonights_games_ml.py
+++ src/nba_tonights_games_ml.py
@@ -4,22 +4,38 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from utils import *
 
-conn = sql_connection('ml_models')
-tonights_games_full = pd.read_sql_query('select * from ml_tonights_games', conn).sort_values('home_team_avg_pts_scored') # the full df
-tonights_games = tonights_games_full.drop(['home_team', 'away_team', 'proper_date', 'outcome'], axis = 1)                # for ml
+conn = sql_connection("ml_models")
+tonights_games_full = pd.read_sql_query(
+    "select * from ml_tonights_games", conn
+).sort_values(
+    "home_team_avg_pts_scored"
+)  # the full df
+tonights_games = tonights_games_full.drop(
+    ["home_team", "away_team", "proper_date", "outcome"], axis=1
+)  # for ml
 
-past_games = pd.read_sql_query('select * from ml_past_games', conn)
-past_games_outcome = past_games['outcome'].to_numpy()
-past_games = past_games.drop(['home_team', 'away_team', 'proper_date', 'outcome'], axis = 1).to_numpy()
+past_games = pd.read_sql_query("select * from ml_past_games", conn)
+past_games_outcome = past_games["outcome"].to_numpy()
+past_games = past_games.drop(
+    ["home_team", "away_team", "proper_date", "outcome"], axis=1
+).to_numpy()
 
 
 clf = LogisticRegression(random_state=0).fit(past_games, past_games_outcome)
 
-tonights_ml = pd.DataFrame(clf.predict_proba(tonights_games)).rename(columns = {0: "away_team_predicted_win_pct", 1: "home_team_predicted_win_pct"})
+tonights_ml = pd.DataFrame(clf.predict_proba(tonights_games)).rename(
+    columns={0: "away_team_predicted_win_pct", 1: "home_team_predicted_win_pct"}
+)
 
-tonights_games_ml = tonights_games_full.reset_index().drop('outcome', axis = 1) # reset index so predictions match up correctly
+tonights_games_ml = tonights_games_full.reset_index().drop(
+    "outcome", axis=1
+)  # reset index so predictions match up correctly
 
-tonights_games_ml['home_team_predicted_win_pct'] = tonights_ml['home_team_predicted_win_pct'].round(3)
-tonights_games_ml['away_team_predicted_win_pct'] = tonights_ml['away_team_predicted_win_pct'].round(3)
+tonights_games_ml["home_team_predicted_win_pct"] = tonights_ml[
+    "home_team_predicted_win_pct"
+].round(3)
+tonights_games_ml["away_team_predicted_win_pct"] = tonights_ml[
+    "away_team_predicted_win_pct"
+].round(3)
 
 write_to_sql(conn, "tonights_games_ml", tonights_games_ml, "append")
