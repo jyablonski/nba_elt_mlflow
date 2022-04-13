14e84fc6a2142d32ed336f5dd0d0bda392c2bcd0
jyablonski9 <jyablonski9@gmail.com>
2022-04-13 12:24:37 -0500

    Bump version: 1.3.2 → 1.3.3

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ logging.basicConfig(
     handlers=[logging.FileHandler("logs/example.log"), logging.StreamHandler()],
 )
 
-logging.info("STARTING NBA ELT MLFLOW Version: 1.3.2")
+logging.info("STARTING NBA ELT MLFLOW Version: 1.3.3")
 
 conn = sql_connection("ml_models")
 
