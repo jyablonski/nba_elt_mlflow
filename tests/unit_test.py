fdae9a37c2cdc7d74a05a2070a2abf25ad13d07d
jyablonski9 <jyablonski9@gmail.com>
2022-04-13 12:31:40 -0500

    fix unit tests

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from joblib import load
 
 import numpy as np
@@ -8,6 +9,8 @@ from sklearn.linear_model import LogisticRegression
 from src.utils import calculate_win_pct
 
 def test_calculate_win_pct(ml_data, full_df, ml_model):
+    # 2022-04-13 - i made a change to only check for today's date, so just do this.
+    full_df['proper_date'] = datetime.now().date()
     df = calculate_win_pct(ml_data, full_df, ml_model)
 
     assert len(df) == 8
