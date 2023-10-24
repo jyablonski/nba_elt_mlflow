a0353f67e646a26c02138a446e731a69bd168dd9
jyablonski9 <jyablonski9@gmail.com>
2023-10-24 12:44:27 -0500

    remopve dat

--- docker/postgres_bootstrap.sql
+++ docker/postgres_bootstrap.sql
@@ -39,41 +39,6 @@ VALUES ('Boston Celtics', 'Chicago Bulls', '-160', '200', current_date, 8, 3, 11
        ('Denver Nuggets','Los Angeles Lakers','-200.0','165.0',current_date,4,133,115.4,111.0,0.646,0.5,2.0,14,156,116.3,115.4,0.524,0.8,2.0,NULL),
 	   ('Golden State Warriors','Phoenix Suns','-160.0','130.0',current_date,12,164,118.1,116.5,0.537,0.8,2.0,10,165,113.7,112.2,0.549,0.7,2.0,NULL);
 
-DROP TABLE IF EXISTS ml_tonights_games_prod;
-CREATE TABLE IF NOT EXISTS ml_tonights_games_prod (
-	home_team text NULL,
-	away_team text NULL,
-	home_moneyline numeric NULL,
-	away_moneyline numeric NULL,
-	proper_date date NULL,
-	home_team_rank int8 NULL,
-    home_days_rest int4 NULL,
-	home_team_avg_pts_scored numeric NULL,
-	home_team_avg_pts_scored_opp numeric NULL,
-	home_team_win_pct numeric NULL,
-	home_team_win_pct_last10 numeric NULL,
-	home_is_top_players numeric NULL,
-	away_team_rank int8 NULL,
-    away_days_rest int4 NULL,
-	away_team_avg_pts_scored numeric NULL,
-	away_team_avg_pts_scored_opp numeric NULL,
-	away_team_win_pct numeric NULL,
-	away_team_win_pct_last10 numeric NULL,
-	away_is_top_players numeric NULL,
-	outcome int4 NULL
-);
-
-
-INSERT INTO ml_tonights_games_prod (home_team, away_team, home_moneyline, away_moneyline, proper_date, home_team_rank,
-                               home_days_rest, home_team_avg_pts_scored, home_team_avg_pts_scored_opp, home_team_win_pct,
-                               home_team_win_pct_last10, home_is_top_players, away_team_rank, away_days_rest,
-                               away_team_avg_pts_scored, away_team_avg_pts_scored_opp, away_team_win_pct,
-                               away_team_win_pct_last10, away_is_top_players, outcome)
-VALUES ('Boston Celtics', 'Chicago Bulls', '-160', '200', current_date, 8, 3, 116.3, 112.4, 0.75, 0.63, 2, 14, 0, 115.3, 117.2, 0.45, 0.48, 2, null),
-       ('Golden State Warriors', 'New Orleans Pelicans', '-250', '410', current_date, 4, 1, 118.3, 115.1, 0.66, 0.75, 2, 14, 2, 114.3, 112.2, 0.56, 0.44, 1, null),
-       ('Denver Nuggets','Los Angeles Lakers','-200.0','165.0',current_date,4,133,115.4,111.0,0.646,0.5,2.0,14,156,116.3,115.4,0.524,0.8,2.0,NULL),
-	   ('Golden State Warriors','Phoenix Suns','-160.0','130.0',current_date,12,164,118.1,116.5,0.537,0.8,2.0,10,165,113.7,112.2,0.549,0.7,2.0,NULL);
-
 -- table that the ml pipeline writes predictions to 
 DROP TABLE IF EXISTS tonights_games_ml;
 CREATE TABLE IF NOT EXISTS tonights_games_ml
