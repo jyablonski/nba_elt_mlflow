a9c6445f43f4a8934417585e4ce23f1f0a08d567
jacob <jyablonski9@gmail.com>
2023-10-25 09:00:49 -0500

    ML Pipeline Prod Release v1.6.2 (#19)
    ### Description
    Fixed Path issue w/ Log Model
    
    ## Added
    - None
    
    ## Updated
    - Log Model Name
    - Moved `pull_request_template.md` so it'll work now ;)
    
    ## Deleted
    - None

--- src/app.py
+++ src/app.py
@@ -19,7 +19,7 @@ logging.basicConfig(
     handlers=[logging.FileHandler("logs/example.log"), logging.StreamHandler()],
 )
 
-logging.info("STARTING NBA ELT MLFLOW Version: 1.6.1")
+logging.info("STARTING NBA ELT MLFLOW Version: 1.6.2")
 
 conn = sql_connection("ml_models")
 feature_flags = get_feature_flags(conn)
@@ -33,7 +33,7 @@ if feature_flag_bool is False:
 tonights_games_full = pd.read_sql_query(
     "select * from ml_tonights_games", conn
 ).sort_values("home_team_avg_pts_scored")
-log_regression_model = load("log_model.joblib")
+log_regression_model = load("src/log_model.joblib")
 
 tonights_games_ml = calculate_win_pct(
     full_df=tonights_games_full, ml_model=log_regression_model
