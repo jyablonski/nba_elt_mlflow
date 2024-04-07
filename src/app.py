4a970e96f2da74c8fcc1819f3dfa0e5637b35914
jyablonski9 <jyablonski9@gmail.com>
2024-04-07 09:24:07 -0500

    Bump version: 1.6.6 → 1.6.7

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ logging.basicConfig(
 )
 
 if __name__ == "__main__":
-    logging.info("Starting NBA ELT MLFLOW Version: 1.6.6")
+    logging.info("Starting NBA ELT MLFLOW Version: 1.6.7")
 
     conn = sql_connection("ml_models")
     with conn.begin() as connection:
@@ -41,4 +41,4 @@ if __name__ == "__main__":
         )
         write_to_sql(con=connection, table_name="tonights_games_ml", df=tonights_games_ml, table_type="append")
     
-    logging.info("Finished NBA ELT MLFLOW Version: 1.6.6")
+    logging.info("Finished NBA ELT MLFLOW Version: 1.6.7")
